d885ccbc0e92612719e68a0b4c4b07e11d555cd3
AvtEstudos <andretamayoshi@gmail.com>
2020-10-25 12:58:54 -0300

    Proximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,19 @@
 import React, { Profiler } from 'react';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
-import userEvent from '@testing-library/user-event';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
@@ -31,6 +39,33 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://app-gobarber-avt.s3.us-east-2.amazonaws.com/f016b51590e59b5c9130-Eddard_Stark.png"
+                alt="André Vital"
+              />
+              <strong>André Vital</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Calendar />
+        </Schedule>
+      </Content>
+
     </Container>
   );
 };
