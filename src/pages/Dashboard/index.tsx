95c26cd5ad09d672ebc4ca0223334b2c775a4cb9
AvtEstudos <andretamayoshi@gmail.com>
2020-10-27 08:16:03 -0300

    Calendario e estilizacao

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
-import React, { Profiler } from 'react';
+import React, { useCallback, useState } from 'react';
+import DayPicker, {DayModifiers} from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import {
   Container,
@@ -8,7 +10,9 @@ import {
   Content,
   Schedule,
   Calendar,
-  NextAppointment
+  NextAppointment,
+  Section,
+  Appointment,
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
@@ -16,6 +20,14 @@ import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available){
+      setSelectedDate(day);
+    }
+  }, []);
+
   const { signOut, user } = useAuth();
   return (
     <Container>
@@ -62,10 +74,89 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
-          <Calendar />
-        </Schedule>
-      </Content>
 
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://app-gobarber-avt.s3.us-east-2.amazonaws.com/f016b51590e59b5c9130-Eddard_Stark.png"
+                  alt="André Vital"
+                />
+                <strong>André Vital</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://app-gobarber-avt.s3.us-east-2.amazonaws.com/f016b51590e59b5c9130-Eddard_Stark.png"
+                  alt="André Vital"
+                />
+                <strong>André Vital</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://app-gobarber-avt.s3.us-east-2.amazonaws.com/f016b51590e59b5c9130-Eddard_Stark.png"
+                  alt="André Vital"
+                />
+                <strong>André Vital</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+              <div>
+                <img
+                  src="https://app-gobarber-avt.s3.us-east-2.amazonaws.com/f016b51590e59b5c9130-Eddard_Stark.png"
+                  alt="André Vital"
+                />
+                <strong>André Vital</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+        </Schedule>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: {daysOfWeek: [ 1, 2, 3, 4, 5 ]},
+            }}
+           selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+              'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro',
+            ]}
+
+          />
+        </Calendar>
+      </Content>
     </Container>
   );
 };
